729672c9b1cddc09aced70e4320d337d4e362494
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2020-10-16 13:54:21 +0530

    implemented pagination and changed empty cart and empty orders ui

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,13 +6,33 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
+const ITEMS_PER_PAGE = 4;
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
+
+  let page = +req.query.page;
+  let totalItems;
+  if(!page){
+    page = 1;
+  }
+
+  Product.find().countDocuments().then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  })
     .then((products) => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE) 
       });
     })
     .catch((err) => {
@@ -40,12 +60,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
-    .then((products) => {
+  let page = +req.query.page;
+  let totalItems;
+  if(!page){
+    page = 1;
+  }
+
+  Product.find().countDocuments().then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  }).then((products) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE) 
       });
     })
     .catch((err) => {
@@ -76,6 +112,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+
+  if(!req.session.isLoggedIn){
+    return res.redirect('/login');
+  }
+
   Product.findById(prodId)
     .then((product) => {
       return req.user.addToCart(product);
